cad06cdd59eacac40ff68a155ef96f24ea9fbc3d
d-caio <diascaio@proton.me>
2026-03-05 22:20:26 -0300

    Lote 1.2, Ex 17 e 18

---
+++ lote1.2/lote1-ex17-mod_proc/Main.java
@@ -0,0 +1,80 @@
+/*
+OBJETIVO: Calcule a quantidade de litros gastos em uma viagem, sabendo que o automóvel faz 12 km/l. Receber o tempo de percurso e a velocidade média.
+PROGRAMADOR: Caio Dias (d-caio).
+DATA: 05/03/2026
+*/
+
+import javax.swing.*;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Main {
+    private static final double KM_L = 12;
+
+    private static final int MAX_TRIES = 5;
+    
+    private static double l, t, v;
+
+    public static void main(String args[]) {
+        int tries = 0;
+        
+        while (true) {
+            if (tries == MAX_TRIES) {
+                JOptionPane.showMessageDialog(null, "Máximo de tentativas atingido. O programa será encerrado.", "Tentativa " + tries + "/" + MAX_TRIES, JOptionPane.ERROR_MESSAGE);
+
+                return;
+            }
+
+            try {
+                String entrada = JOptionPane.showInputDialog("Tempo de percurso em horas: ");
+
+                if (entrada == null) {
+                    JOptionPane.showMessageDialog(null, "Programa encerrado pelo usuário.", "Fim", JOptionPane.WARNING_MESSAGE);
+
+                    return;
+                }
+
+                t = Double.parseDouble(entrada);
+
+                if (t <= 0)
+                    throw new IllegalArgumentException("O tempo de percurso precisa ser maior que 0 h.");
+            
+                entrada = JOptionPane.showInputDialog("Velocidade média em km/h: ");
+
+                if (entrada == null) {
+                    JOptionPane.showMessageDialog(null, "Programa encerrado pelo usuário.", "Fim", JOptionPane.WARNING_MESSAGE);
+
+                    return;
+                }
+
+                v = Double.parseDouble(entrada);
+
+                if (v <= 0)
+                    throw new IllegalArgumentException("A velocidade média precisa ser maior que 0 km/h.");
+
+                calcularLitrosGastos();
+            
+                String lFormatado = NumberFormat.getInstance(Locale.forLanguageTag("pt")).format(l);
+
+                JOptionPane.showMessageDialog(null, "Serão gastos " + lFormatado + "l de combustível na viagem.", "Resultado", JOptionPane.INFORMATION_MESSAGE);
+
+                return;
+
+            } catch (NumberFormatException e) {
+                tries++;
+
+                if (tries < MAX_TRIES)
+                    JOptionPane.showMessageDialog(null, "Os valores precisam ser números reais.", "Tentativa " + tries + "/" + MAX_TRIES, JOptionPane.ERROR_MESSAGE);
+
+            } catch (IllegalArgumentException e) {
+                tries++;
+
+                if (tries < MAX_TRIES)
+                    JOptionPane.showMessageDialog(null, e.getMessage(), "Tentativa " + tries + "/" + MAX_TRIES, JOptionPane.ERROR_MESSAGE);
+
+            }
+        }
+    }
+
+    private static void calcularLitrosGastos() { l = ((v * t) / KM_L); }
+}
---
+++ lote1.2/lote1-ex18-mod_proc/Main.java
@@ -0,0 +1,79 @@
+/*
+OBJETIVO: Receba 2 valores inteiros. Calcule e mostre o resultado da diferença do maior pelo menor valor.
+PROGRAMADOR: Caio Dias (d-caio).
+DATA: 05/03/2026
+*/
+
+import java.util.*;
+import javax.swing.*;
+import java.text.*;
+
+public class Main {
+    private static int num1, num2;
+
+    private static final int MAX_TRIES = 5;
+
+    public static void main(String[] args) {
+        int tries = 0;
+
+        Scanner entrada = new Scanner(System.in);
+
+        while (true) {
+            if (tries == MAX_TRIES) {
+                System.out.println("Erro " + tries + "/" + MAX_TRIES + " - Máximo de tentativas atingido. O programa será encerrado.");
+                entrada.close();
+                return;
+            }
+
+            try {
+                System.out.print("Primeiro número: ");
+                num1 = entrada.nextInt();
+
+                System.out.print("Segundo número: ");
+                num2 = entrada.nextInt();
+
+                diferencaMaiorPeloMenor();
+
+                entrada.close();
+
+                return;
+
+            } catch (InputMismatchException e) {
+                tries++;
+
+                entrada.nextLine();
+
+                if (tries < MAX_TRIES)
+                    System.out.println("Erro " + tries + "/" + MAX_TRIES + " - Os valores pecisam ser números inteiros.");
+
+            } catch (IllegalArgumentException e) {
+                tries++;
+
+                entrada.nextLine();
+
+                if (tries < MAX_TRIES)
+                    System.out.println("Erro " + tries + "/" + MAX_TRIES + " - " + e.getMessage());
+
+            }
+        }
+    }
+
+    private static void diferencaMaiorPeloMenor() {
+        if (num1 == num2)
+            throw new IllegalArgumentException("Os números precisam ser diferentes entre si.");
+
+        NumberFormat formatador = NumberFormat.getInstance(Locale.forLanguageTag("pt"));
+
+        String num1Formatado = formatador.format(num1);
+        String num2Formatado = formatador.format(num2);
+        String difNum1Maior = formatador.format(num1 - num2);
+        String difNum2Maior = formatador.format(num2 - num1);
+
+        if (num1 > num2)
+            System.out.println(num2 < 0 ? num1Formatado + " - " + "(" + num2Formatado + ")" + " = " + difNum1Maior : num1Formatado + " - " + num2Formatado + " = " + difNum1Maior);
+
+        else
+            System.out.println(num1 < 0 ? num2Formatado + " - " + "(" + num1Formatado + ")" + " = " + difNum2Maior : num2Formatado + " - " + num1Formatado + " = " + difNum2Maior);
+
+    }
+}
