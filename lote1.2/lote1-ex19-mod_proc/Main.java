00540a41f79bfaa3b34141b2134ec6ad5508a750
d-caio <diascaio@proton.me>
2026-03-07 19:50:19 -0300

    Lote 1.2, Ex 19

---
+++ lote1.2/lote1-ex19-mod_proc/Main.java
@@ -0,0 +1,38 @@
+/*
+OBJETIVO: Receba 2 valores reais. Calcule e mostre o maior deles.
+PROGRAMADOR: Caio Dias (d-caio).
+DATA: 07/03/2026
+*/
+
+public class Main {
+    private static double num1, num2, maior, menor;
+
+    public static void main(String[] args) {
+        try {
+            if (args.length != 2)
+                throw new ArrayIndexOutOfBoundsException("Erro: O programa precisa ser executado com exatamente dois argumentos.");
+
+            num1 = Double.parseDouble(args[0]);
+            num2 = Double.parseDouble(args[1]);
+
+            determinarMaiorMenor();
+
+            System.out.println(maior + " > " + menor);
+            
+        } catch (NumberFormatException e) {
+            System.out.println("Erro: Os valores precisam ser números.");
+            
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    private static void determinarMaiorMenor() {
+        if (num1 == num2)
+            throw new IllegalArgumentException("Erro: Os números precisam ser diferentes.");
+
+        maior = Math.max(num1, num2);
+        menor = Math.min(num1, num2);
+    }
+}
